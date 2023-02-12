d74e17c020d0c42b44d1c99a3ff3e3c1ee9e760a
HRS-Computer <3474006766@qq.com>
2023-02-12 19:38:23 +0800

    update

---
+++ src/main/java/io/github/gdpl2112/Main.java
@@ -0,0 +1,11 @@
+package io.github.gdpl2112;
+
+/**
+ * @author github-kloping
+ * @date 2023-02-12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("这是一个依赖包的仓库\nThis is a repository that depends on packages");
+    }
+}
